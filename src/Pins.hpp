7a999f23deb899d6a9789e27717cf0d5b841bab4
Peblo Schneider <69631510+PabloSchneider@users.noreply.github.com>
2025-01-09 13:27:37 +0100

    Update Pins.hpp

--- src/Pins.hpp
+++ src/Pins.hpp
@@ -8,3 +8,9 @@
 #define LORA_MISO 19
 #define LORA_MOSI 27
 #define LORA_BAND 433E6
+
+#define TFT_CS         25  //case select connect to pin 5
+#define TFT_RST        13 //reset connect to pin 15
+#define TFT_DC         12 //AO connect to pin 32  (not sure that this is really used)  try pin 25
+#define TFT_MOSI       23 //Data = SDA connect to pin // 
+#define TFT_SCK   
