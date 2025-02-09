f86a054a1f0d74b7cf82fd2d5af8cf1813c86fd3
Pablo Schneider <pablo.schneider@chaos1.de>
2025-02-09 20:24:56 +0100

    ref: config erklärungen

--- src/configValues.hpp
+++ src/configValues.hpp
@@ -1,29 +1,34 @@
 #pragma once 
-//Display
+
+//Display // DO NOT CHANGE
 #define SCREEN_WIDTH  128
 #define SCREEN_HEIGHT 160
-#define VIEW_DISTANCE 100 // in meters
-#define FIELD_RADIUS  300 // Radius des Spielfelds in Metern
 
-//Communication
-#define PAYLOAD_SIZE 100
+#define VIEW_DISTANCE 350 // Sichtweite in Metern
+#define FIELD_RADIUS  3000 // Radius des Spielfelds in Metern
+
+//Communication // DO NOT CHANGE
+#define PAYLOAD_SIZE 100 
 #define HEADER_SIZE 3
 #define QUEUE_SIZE 10 // Maximum number of messages in the queue
 
 
 //Game logic
-#define MAX_PLAYERS 3
-#define CANCEL_SEARCH_AFTER (10* 60 * 1000)
-#define RESEND_JOINING_MESSAGE_INTERVAL (30 * 1000)
-#define REMOVE_PLAYER_NOT_ACCEPTING_AFTER (10 * 1000)
-#define REMOVE_PLAYER_NOT_ANSWERING_AFTER (5 * 60 * 1000)
-#define SEND_POS_INTERVAL (10 * 1000)
-#define GAME_DURATION (120 * 60 * 1000)
-#define HUNTER_REVEAL (1 * 60 * 1000)
+#define MAX_PLAYERS 3   // Maximale Anzahl an Spielern
+#define GAME_DURATION (120 * 60 * 1000) // Wie lange das Spiel dauern soll
+#define HUNTER_REVEAL (1 * 60 * 1000) // Zeit nachdem der Jäger enttarnt wird
+#define SEND_POS_INTERVAL (60 * 1000)  // Wie oft die Position gesendet wird
 
-//Button
-#define BUTTON_COOLDOWN 1000 //5sec nur zum testen sonnst 1000
+
+#define CANCEL_SEARCH_AFTER (10* 60 * 1000) // Wann die Suche abgebrochen wird
+#define RESEND_JOINING_MESSAGE_INTERVAL (30 * 1000) // Interval, wann die Anfrage erneut gesendet wird
+#define REMOVE_PLAYER_NOT_ACCEPTING_AFTER (10 * 1000) // Wann ein Spieler entfernt wird, wenn er die Anfrage nicht annimmt
+#define REMOVE_PLAYER_NOT_ANSWERING_AFTER (5 * 60 * 1000) // Wann ein Spieler entfernt wird, wenn er nicht antwortet
+
+
 
 // GPS
-#define WAIT_DURATION_GPS_INIT (10 * 60 * 1000)
+#define WAIT_DURATION_GPS_INIT (10 * 60 * 1000) //Wie lange auf GPS-Signal gewartet wird
 
+//Button
+#define BUTTON_COOLDOWN 1000 //DO NOT CHANGE
